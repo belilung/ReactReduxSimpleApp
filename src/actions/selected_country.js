e660d5e58190daef64679845446919709a91f40d
Bobnyuk Andrew <bobnyuka.a@gmail.com>
2016-05-17 10:11:15 +0300

    Update selected_country.js

--- src/actions/selected_country.js
+++ src/actions/selected_country.js
@@ -4,8 +4,7 @@ export const COUNTRY_SELECTED = 'COUNTRY_SELECTED';
 const ROOT_URL = `http://macho.wow4.ru/api/web/v1/countries/`;
 
 export function selectCountry(country_key) {
-  // selectCountry is an ActionCreator, it needs to return an action,
-  // an object with a type property.
+
   const url = ROOT_URL + country_key;
 
   const request = axios.get(url);
